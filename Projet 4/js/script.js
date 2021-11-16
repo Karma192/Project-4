894b4e9d9e12d7fa79e0c5c0613d80c487f5ca7c
Karma <agrandpaul@gaming.tech>
2021-11-16 10:56:16 +0100

    ca avance

---
+++ Projet 4/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, options);
+});
+
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+});
