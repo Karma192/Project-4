8a567ef6b346057ba03ca55f6d5b98bc42a32b24
Karma <agrandpaul@gaming.tech>
2021-11-17 12:45:21 +0100

    ui

--- Projet 4/js/script.js
+++ Projet 4/js/script.js
@@ -16,21 +16,6 @@ $('.carousel.carousel-slider').carousel({
     indicators: true
 });
 
-//The Modal
-var modal = document.getElementById("Modal");
-var btn = document.getElementById("Button");
-var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+$(document).ready(function(){
+   $('.modal').modal();
+ });
